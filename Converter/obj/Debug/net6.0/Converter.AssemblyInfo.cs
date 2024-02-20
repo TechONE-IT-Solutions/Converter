3b749e30c11425b7edf57649002220afb1f0fea3
HAKIMABEN12 <124052578+HAKIMABEN12@users.noreply.github.com>
2024-02-20 11:28:06 +0000

    update

--- Converter/obj/Debug/net6.0/Converter.AssemblyInfo.cs
+++ Converter/obj/Debug/net6.0/Converter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Converter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a620a3d1b9be2bea73e15ede78804c7655fba191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f9b3e41e68767dcf15308971d61d4546001f84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Converter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Converter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
